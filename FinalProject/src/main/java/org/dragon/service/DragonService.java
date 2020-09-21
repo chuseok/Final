d196d78fa9ee3bd2108c7641ee7c587739c247b4
lhj8390 <lhj8390@naver.com>
2020-09-21 09:53:30 +0900

    android/dragon mapping

--- FinalProject/src/main/java/org/dragon/service/DragonService.java
+++ FinalProject/src/main/java/org/dragon/service/DragonService.java
@@ -3,6 +3,7 @@ package org.dragon.service;
 import java.util.HashMap;
 import java.util.List;
 
+import org.dragon.domain.CollectionVO;
 import org.dragon.domain.DragonVO;
 import org.login.domain.MemberVO;
 
@@ -18,11 +19,15 @@ public interface DragonService {
 	public List<Integer> getDragonId(int productId);
 	public boolean updateEquip(DragonVO dragonVO);
 	public HashMap<String, String> getImageByLevel(int dragonId);
-	//public int getBackgroundId(String userId);
+	public List<CollectionVO> getListByDragonLists();
+	public String getLevel1Name(String productImage);
+	public String getLevel2Name(String productImage);
+	public String getLevel3Name(String productImage);
 	public boolean updateBackground(DragonVO dragonVO);
 	//public boolean updateFoodValue(DragonVO dragonVO);
 	public boolean delete(DragonVO dragonVO);
 	public int getProductId(int dragonId);
 	public boolean reviveDragon(String userId);
+	public DragonVO getDragonByDragonId(DragonVO dragonVO);
 
 }
