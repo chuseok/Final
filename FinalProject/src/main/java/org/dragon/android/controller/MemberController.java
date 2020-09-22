b2824c13e436849002b7a9963fbed00dba999553
lhj8390 <lhj8390@naver.com>
2020-09-22 09:37:19 +0900

    member controller 오류 수정

--- FinalProject/src/main/java/org/dragon/android/controller/MemberController.java
+++ FinalProject/src/main/java/org/dragon/android/controller/MemberController.java
@@ -27,22 +27,16 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 public class MemberController {
 
-<<<<<<< HEAD
     MemberService memberService;
     
-    @GetMapping(value = "/getList")
-    public List<Map<String, String>> get(HttpServletRequest request) {
-=======
-	MemberService memberService;
 	
 	@GetMapping(value = "/getList")
 	public List<Map<String, String>> get(HttpServletRequest request) {
->>>>>>> 1facb42c8249a5f43dd427f9adbfbe31edb65445
+
         List<MemberVO> MemList = new ArrayList<MemberVO>();
         List<Map<String, String>> result = new ArrayList<Map<String, String>>();
         
         MemList = memberService.getList();
-<<<<<<< HEAD
         
         
         for(int i=0;i<MemList.size();i++) {
@@ -58,62 +52,7 @@ public class MemberController {
         return result;
         
     }
-    
-    @PostMapping(value = "/signup")
-    public Map<String, String> register(HttpServletRequest request) {
-        
-        String userName = request.getParameter("userName");
-        String userId = request.getParameter("userId");
-        String userPwd = request.getParameter("userPwd");
-        String userEmail = request.getParameter("userEmail");
-        String userPhone = request.getParameter("userPhone");
-        String userBirth = request.getParameter("userBirth");
-        
-        MemberVO member = new MemberVO();
-        member.setUserName(userName);
-        member.setUserId(userId);
-        member.setUserPwd(userPwd);
-        member.setEmail(userEmail);
-        member.setPhoneNo(userPhone);
-        member.setBirthday(userBirth);
-        
-        AuthVO auth = new AuthVO();
-        auth.setAuth("ROLE_MEMBER");
-        auth.setUserId(userId);
-        
-        System.out.println(member.getUserName());
-        Map<String, String> map = new HashMap<String, String>();
 
-        int insertMemCount = memberService.register(member);
-        int insertAuthCount = memberService.register(auth);
-        
-        if(insertMemCount == 1 && insertAuthCount == 1) {
-            map.put("insertResult", "success");
-        } else {
-            map.put("insertResult", "error");
-        }
-        
-        return map;
-        
-    }
-    
-}
-=======
-		
-        
-        for(int i=0;i<MemList.size();i++) {
-        	Map<String, String> map = new HashMap<String, String>();
-        	String userId = MemList.get(i).getUserId();
-        	String userName = MemList.get(i).getUserName();
-        	map.put("userId", userId);
-        	map.put("userName", userName);
-        	
-        	result.add(map);
-        }
-		
-		return result;
-		
-	}
 	
 	@PostMapping(value = "/signup")
 	public Map<String, String> register(HttpServletRequest request) {
@@ -154,4 +93,3 @@ public class MemberController {
 	}
 	
 }
->>>>>>> 1facb42c8249a5f43dd427f9adbfbe31edb65445
